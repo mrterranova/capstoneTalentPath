ddec1abf2a99d226b97bd2913aba586e86cd6e91
Michal Terranova <michalterranova@mail.com>
2020-05-20 11:29:19 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql");
+const dbConfig = require("./config/keys");
+const api = require("./routes/api");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const port = process.env.PORT || 5000;  
+const connection = mysql.createConnection(dbConfig);  
+
+//parts that I am adding in...
+// app.use()
+
+connection.connect(err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("sucess");
+  }
+});
+
+app.use("/api", api);
+
+//
+app.listen(port, () => {
+  console.log("Server Listening on port " + port);
+});
